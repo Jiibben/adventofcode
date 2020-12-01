e266cdc9287edc09c12a60281bb8357a60805d6d
Jiibben <allan.burnier@gmail.com>
2020-12-01 23:28:57 +0100

    day1 part2 finished

--- day1.py
+++ day1.py
@@ -1,15 +1,9 @@
-with open("input.txt", "r") as f:
-    d = f.readlines()
-ex_report = list(map(lambda x:int(x.replace("\n", "")), d))
-rep = 0
-a = ex_report[rep]
-found = False
-while not found:
-    rep += 1
-    for i in ex_report:
-        d = a + i
-        if d == 2020:
-            print("The answer is {}".format(a*i))
-            found = True
-            break
-    a = ex_report[rep]
+def lfromfile(file):
+    return list(map(lambda x: int(x.replace("\n", "")), open(file,"r").readlines()))
+
+def find_sum(list_):
+    for i in list_:
+        if 2020 - i in list_:
+            return i * (2020-i)
+
+print(find_sum(lfromfile("input.txt")))
--- day1part2.py
+++ day1part2.py
@@ -1,32 +1,9 @@
-with open("input.txt", "r") as f:
-    d = f.readlines()
-ex_report = list(map(lambda x: int(x.replace("\n", "")), d))
+def lfromfile(file):
+    return list(map(lambda x: int(x.replace("\n", "")), open(file,"r").readlines()))
 
+def find_sum(list_):
+    for i in list_:
+        if 2020 - i in list_:
+            return i * (2020-i)
 
-def permute(list, s):
-    if list == 1:
-        return s
-    else:
-        return [str(y) + " " + str(x)
-                for y in permute(1, s)
-                for x in permute(list - 1, s)
-                ]
-
-
-def permuting(list, s):
-    if list == 1:
-        return s
-    else:
-        return [x + y for y in permuting(1, s) for x in permuting(list - 1, s)
-                ]
-
-
-a = permute(3, ex_report)
-b = permuting(3, ex_report)
-
-for i in b:
-    if i == 2020:
-        c = a[b.index(i)]
-        break
-
-print(c)
+print(find_sum(lfromfile("input.txt")))
