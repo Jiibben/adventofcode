e985aaa19ef5472e068ee922418e4f8751aca141
Jiibben <allan.burnier@gmail.com>
2020-12-01 23:39:55 +0100

    Cleaned day1part2.py

--- day1part2.py
+++ day1part2.py
@@ -1,9 +1,19 @@
-def lfromfile(file):
-    return list(map(lambda x: int(x.replace("\n", "")), open(file,"r").readlines()))
+with open("input.txt", "r") as f:
+    d = f.readlines()
+ex_report = list(map(lambda x: int(x.replace("\n", "")), d))
+
 
 def find_sum(list_):
-    for i in list_:
-        if 2020 - i in list_:
-            return i * (2020-i)
+    found = False
+    rep = 0
+    while not found:
+        a = list_[rep]
+        for b in list_:
+            if 2020 - (a + b) in ex_report:
+                c = 2020 - (a + b)
+                found = True
+                return a * b * c
+        rep += 1
 
-print(find_sum(lfromfile("input.txt")))
+
+print(find_sum(ex_report))
