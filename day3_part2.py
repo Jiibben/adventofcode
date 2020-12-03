9558bd34be50358b6ac9ec7f0cb64dfa224c57da
Jiibben <allan.burnier@gmail.com>
2020-12-03 13:49:34 +0100

    day3part2 in work

--- day3_part2.py
+++ day3_part2.py
@@ -10,14 +10,12 @@ a = getlist("input3.txt")
 def move(x, y, map):
     b = 0
     c = 0
-    lol = y
     for i in map:
-        if b + x >= len(i):
+        if b + x > len(i):
             b = b - len(i)
-        b += x
         if i[b] == "#":
             c += 1
-
+        b += x
     return c
 
 print(move(3, 1, a))
