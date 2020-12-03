fde46c2aeef5b9af39d2d7ff1df69cedccc3e24b
Jiibben <allan.burnier@gmail.com>
2020-12-03 14:12:54 +0100

    day3part2 in work

--- day3_part2.py
+++ day3_part2.py
@@ -5,17 +5,33 @@ def getlist(file):
 
 
 a = getlist("input3.txt")
+search = {
+    "right": [1, 3, 5, 7, 1],
+    "down": [1, 1, 1, 1, 2]
+}
 
 
 def move(x, y, map):
     b = 0
     c = 0
+
     for i in map:
         if b + x > len(i):
             b = b - len(i)
-        if i[b] == "#":
-            c += 1
+        if map.index(i) % y == 0 and map.index(i) != 0:
+
+            if i[b] == "#":
+                c += 1
         b += x
     return c
 
-print(move(3, 1, a))
+
+result = []
+for z, w in zip(search["right"], search["down"]):
+    result.append(move(z, w, a))
+
+print(result)
+mac = 1
+for t in result:
+    mac = t * mac
+print(mac)
