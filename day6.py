f4fbda295fc8429aec0a40f7cf2eb59bce519000
Jiibben <64984990+Jiibben@users.noreply.github.com>
2020-12-06 16:08:10 +0100

    day6_part2 in progress

--- day6.py
+++ day6.py
@@ -1,48 +1,24 @@
 import itertools
-import string
-def part1(file):
-    total = 0
-    d = []
-    g = []
-    c = [i.replace("\n", "") for i in open(file,"r")]
-    for i in c:
-        if i != "":
-            d.append(list(i))
-        if i == "":
-            g.append(list(set(itertools.chain.from_iterable(d))))
-            d = []
-    for i in g:
-        total += len(i)
-    return total
+import datetime
+
+groups = [line for line in open("input6.txt", "r").read().split("\n\n")]
+
+def timer(f):
+    def wrapper(*kwargs):
+        t1 = datetime.datetime.now().microsecond
+        x = f(*kwargs)
+        t2 = datetime.datetime.now().microsecond
+        print("Function named %s took %s microsecond to run" % (f.__name__, t2-t1))
+        return x
+    return wrapper
 
 
-def part2(file):
-    total = 0
-    d = []
-    g = []
-    c = [i.replace("\n", "") for i in open(file,"r")]
-    for i in c:
-        if i != "":
-            d.append(list(i))
-        if i == "":
-            g.append(d)
-            d = []
-    return g
+@timer
+def part1(data):
+    return list(itertools.accumulate(list(map(lambda x: len(set(itertools.chain.from_iterable(x.split("\n")))), data))))[-1]
 
-
-def check_group_dec(liste_of_list):
-    d = list(string.ascii_lowercase)
-    for x in string.ascii_lowercase:
-        for i in liste_of_list:
-            if x in i:
-                pass
-            elif x not in i:
-                d.pop(d.index(x))
-                break
-    return len(d)
-
-tot = 0
-for i in part2("input6.txt"):
-    tot += check_group_dec(i)
-
-print(tot)
+@timer
+def part2(data):
+    return list(map(lambda d: [list(i) for i in d], map(lambda x: x.split("\n"), data)))
+    
+print(part2(groups))
