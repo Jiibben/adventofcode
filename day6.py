e2a22bef338378a89c21afef6de0191a6703d04f
Jiibben <64984990+Jiibben@users.noreply.github.com>
2020-12-06 16:25:21 +0100

    day6 done

--- day6.py
+++ day6.py
@@ -1,6 +1,6 @@
 import itertools
 import datetime
-
+from string import ascii_lowercase
 groups = [line for line in open("input6.txt", "r").read().split("\n\n")]
 
 def timer(f):
@@ -18,7 +18,21 @@ def part1(data):
     return list(itertools.accumulate(list(map(lambda x: len(set(itertools.chain.from_iterable(x.split("\n")))), data))))[-1]
 
 @timer
-def part2(data):
-    return list(map(lambda d: [list(i) for i in d], map(lambda x: x.split("\n"), data)))
-    
+def part2(data): 
+    tot = 0
+    def check_group_dec(liste_of_list):
+        d = list(ascii_lowercase)
+        for x in ascii_lowercase:
+            for i in liste_of_list:
+                if x in i:
+                    pass
+                elif x not in i:
+                    d.pop(d.index(x))
+                    break
+        return len(d)
+    for i in map(lambda x: x.split("\n"), data):
+        tot += check_group_dec(i)
+    return tot
+
+print(part1(groups))
 print(part2(groups))
