e80f0204d4109608ef684322e744ceec906af0b9
Jiibben <allan.burnier@gmail.com>
2020-12-09 17:06:36 +0100

    day8 part 1 done §

--- day9.py
+++ day9.py
@@ -19,13 +19,39 @@ def get_preamble_number(index, pb_length, dat):
 run = True
 idx = 0
 idx2 = preamble_length
+answer = ""
 while run:
     result = get_preamble_number(idx, preamble_length, data)
     if int(data[idx2]) in result:
         pass
     else:
         print(data[idx2])
+        answer = int(data[idx2])
         run = False
         break
     idx += 1
     idx2 += 1
+
+inde = 0
+start_inde = 0
+ui = 0
+search = answer
+
+while True:
+    try:
+        ui += int(data[inde])
+        inde += 1
+        if ui == search:
+            break
+        elif ui > search:
+            raise IndexError("ERROR")
+    except IndexError:
+        ui = 0
+        start_inde += 1
+        inde = start_inde
+
+ux = []
+for n in range(start_inde, inde):
+    ux.append(int(data[n]))
+
+print(max(ux) + min(ux))
